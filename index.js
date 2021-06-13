292e3ec13739375ca32cb5a93e6e7f8631d2555e
ATUL SINGH <atulsingh7151@gmail.com>
2021-06-14 02:59:10 +0530

    Backend completed, Socket fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express  = require('express');
-const http     = require('http');
+const express       = require('express');
+const http          = require('http');
 const cors          = require('cors');
 const socketio      = require('socket.io');
 const PORT          = process.env.PORT || 5000;
@@ -19,70 +19,20 @@ app.use(
   })  
 );
 
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-require('./models/User');
-require('./services/passport');
-require('./routes/authRoutes')(app);
-
-
 const io = require('socket.io')(server, {
   cors: {
     origin: 'http://localhost:3000',
   }
 });
 
-var sockets = {},
-users = {},
-strangerQueue = false;
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+require('./models/User');
+require('./services/passport');
+require('./services/socket')(io);
+require('./routes/authRoutes')(app);
 
-
-io.on('connection', (socket) => {
-  console.log("We have a new connection!!", socket.id);
-
-  sockets[socket.id] = socket;
-  users[socket.id] = {
-    connectedTo: -1,
-  }
-
-  if(strangerQueue !== false){
-    users[socket.id].connectedTo = strangerQueue;
-    users[strangerQueue].connectedTo = socket.id;
-    socket.emit('conn', users[socket.id].connectedTo);
-    sockets[strangerQueue].emit('conn');
-    strangerQueue = false;
-  } else {
-    strangerQueue = socket.id;
-  }
-  
-  socket.on('sendMessage', (message, callback) => {
-    if (users[socket.id].connectedTo !== -1 && sockets[users[socket.id].connectedTo]) {
-      sockets[users[socket.id].connectedTo].emit('message', message)
-      callback();
-    }
-  });
-
-  socket.on('disconnect', (err) => {
-      console.log("User has disconnected");
-
-      var connTo = (users[socket.id] && users[socket.id].connectedTo);
-      if(connTo === undefined){
-        connTo = -1;
-      }
-      if(connTo !== -1 && sockets[connTo]) {
-        sockets[connTo].emit("disconn", {who: 2, reason: err && err.toString()});
-        users[connTo].connectedTo = -1;
-      }
-
-      delete sockets[socket.id];
-      delete users[socket.id];
-
-      if(strangerQueue === socket.id || strangerQueue === connTo) {
-        strangerQueue = false;
-      }
-  });
-});
 
 server.listen(PORT);
