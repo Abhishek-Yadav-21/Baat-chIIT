7ab1f63d75139d866b8c1f01cc6eca0e9977d759
ATUL SINGH <atulsingh7151@gmail.com>
2021-06-27 19:07:06 +0530

    Homepage completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
-<<<<<<< HEAD
+
 import Home from './components/Homepage/Home';
-=======
 import Chat from './components/Chat/Chat';
->>>>>>> e09dfe3ffe03c5a4ff51bf743d15c7631c658556
+
 
 const App  = () => {
 
